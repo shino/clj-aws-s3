2fd5638a6bcce18f2d226f8e4648c449825b8b04
Reid Draper <reiddraper@gmail.com>
2012-06-22 21:31:44 -0400

    Move to clojure 1.4.0. update java sdk point release

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-aws-s3 "0.3.1"
   :description "Clojure Amazon S3 library."
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.3.6"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.amazonaws/aws-java-sdk "1.3.11"]]
   :plugins [[codox "0.6.1"]])
