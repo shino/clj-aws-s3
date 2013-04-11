d9fb52667478007051fb428ca89db2eaa43cbd1c
Shunichi Shinohara <shino@basho.com>
2013-04-11 16:18:22 +0900

    Update AWS SDK

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.reiddraper/clj-aws-s3 "0.4.0"
   :description "Clojure Amazon S3 library."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.amazonaws/aws-java-sdk "1.3.11"]]
+                 [com.amazonaws/aws-java-sdk "1.4.1"]]
   :plugins [[codox "0.6.1"]])
