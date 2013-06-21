36a412f1718ca53ea1208b279af923acb1195516
Reid Draper <reiddraper@gmail.com>
2013-06-21 17:54:15 -0400

    Prepare for 0.4.3 development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.reiddraper/clj-aws-s3 "0.4.2"
+(defproject com.reiddraper/clj-aws-s3 "0.4.3-SNAPSHOT"
   :description "Clojure Amazon S3 library."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.amazonaws/aws-java-sdk "1.4.7"]]
