a1ff4c86d38aa9e637dd618b878550acb2963456
Reid Draper <reiddraper@gmail.com>
2013-06-21 17:29:18 -0400

    Update Java SDK and roll version to 0.4.2

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject com.reiddraper/clj-aws-s3 "0.4.1"
+(defproject com.reiddraper/clj-aws-s3 "0.4.2"
   :description "Clojure Amazon S3 library."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.amazonaws/aws-java-sdk "1.4.1"]]
+                 [com.amazonaws/aws-java-sdk "1.4.7"]]
   :plugins [[codox "0.6.1"]])
